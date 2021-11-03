ca0b5270b59f9f4892bef9541788b2e61bedc9ae
shalom.js <StrangeFruit@users.noreply.github.com>
2021-11-03 19:42:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ from typing import Optional
 from fastapi import FastAPI
 import uvicorn
 import random
+from deepface import DeepFace
 
 app = FastAPI()
+
+
+
+img1 = "https://i.imgur.com/N0Y5Dvj.jpeg"
+img2 = "https://i.imgur.com/Ekc4ABJ.png"
+result = DeepFace.verify(img1_path = img1, img2_path = img2)
+
 
 @app.get("/")
 def home():
     return {"Hello": "World from FastAPI"}
+
+@app.get("/face/")
+def get_notes() -> dict:
+    return {
+     result
+    }
 
 # get random number between min(default:0) and max(default:9)
 @app.get("/random/")
